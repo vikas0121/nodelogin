6e214bcb04bb95963be19c89768b72371613d975
Vikas Kumar <VikasKumar@policybazaar.com>
2019-12-23 18:07:06 +0530

    implementing salt

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const SALT_WORK_FACTOR = 10;
 
 userSchema = new Schema({
 	email: {
@@ -34,6 +35,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
 	});
 	if (!user)
 		throw new Error('Unable to login!');
+
 	const isMatch = await bcrypt.compare(password, user.password);
 	if (!isMatch)
 		throw new Error("Password does not match!");
@@ -43,7 +45,8 @@ userSchema.statics.findByCredentials = async (email, password) => {
 userSchema.pre("save", async function (next) {
 	let user = this;
 	if (user.isModified("password")) {
-		user.password = await bcrypt.hash(user.password, 8);
+		const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+		user.password = await bcrypt.hash(user.password, salt, null);
 	}
 	next();
 });
